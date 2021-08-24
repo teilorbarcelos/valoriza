624376df58524bf84fe69accec5735e4b19bc8fe
Teilor Souza Barcelos <tsb-@live.com>
2021-08-24 09:26:49 -0300

    routes get changed to post for test

--- src/routes.ts
+++ src/routes.ts
@@ -15,14 +15,14 @@ const complimentController = new ComplimentController()
 router.get('/', homeController.show)
 
 router.post('/user/create', userController.create)
-router.get('/users/list', onlyAuth, userController.list)
+router.post('/users/list', onlyAuth, userController.list)
 router.post('/user/get', onlyAuth, userController.get)
 router.post('/user/auth/login', userController.login)
 
 router.post('/tag/create', onlyAuth, onlyAdmin, tagController.create)
-router.get('/tags/list', onlyAuth, tagController.list)
+router.post('/tags/list', onlyAuth, tagController.list)
 
-router.get('/compliments/list', onlyAuth, complimentController.list)
+router.post('/compliments/list', onlyAuth, complimentController.list)
 router.post('/compliment/create', onlyAuth, complimentController.create)
 
 export {router}
