6e503f27882909d4d4e7dc9201401ab67f298ea3
Teilor Souza Barcelos <tsb-@live.com>
2021-08-24 09:33:25 -0300

    routes returned to get, same problem with post

--- src/routes.ts
+++ src/routes.ts
@@ -15,14 +15,14 @@ const complimentController = new ComplimentController()
 router.get('/', homeController.show)
 
 router.post('/user/create', userController.create)
-router.post('/users/list', onlyAuth, userController.list)
+router.get('/users/list', onlyAuth, userController.list)
 router.post('/user/get', onlyAuth, userController.get)
 router.post('/user/auth/login', userController.login)
 
 router.post('/tag/create', onlyAuth, onlyAdmin, tagController.create)
-router.post('/tags/list', onlyAuth, tagController.list)
+router.get('/tags/list', onlyAuth, tagController.list)
 
-router.post('/compliments/list', onlyAuth, complimentController.list)
+router.get('/compliments/list', onlyAuth, complimentController.list)
 router.post('/compliment/create', onlyAuth, complimentController.create)
 
 export {router}
